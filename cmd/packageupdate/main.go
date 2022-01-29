2dd0d35b24853e533784632929cb64a00e123c22
Graham Spiers <graham.spiers@verizon.net>
2022-01-29 11:35:05 -0500

    Clean up code.

--- cmd/packageupdate/main.go
+++ cmd/packageupdate/main.go
@@ -6,14 +6,11 @@ import (
 	"crypto/sha1"
 	"flag"
 	"fmt"
-	"log"
-
 	"io/ioutil"
-	//"log"
 	"os"
+	"path/filepath"
 
 	ap "google.golang.org/api/androidpublisher/v3"
-	"path/filepath"
 
 	apt "github.com/napcatstudio/androidpubtools/androidpub"
 	"github.com/napcatstudio/translate/xlns"
@@ -58,10 +55,6 @@ func main() {
 		"images", defaultShotsDir,
 		"Images directory.",
 	)
-	//updateLimit := flag.Int(
-	//	"limit", defaultUpdateLimit,
-	//	"Limit update to this number of locales (-1 = all).",
-	//)
 	textOnly := flag.Bool(
 		"textonly", false,
 		"Only update text.",
@@ -79,7 +72,7 @@ func main() {
 	if flag.NArg() != 1 {
 		usage(fmt.Errorf("wrong number of arguments"))
 	}
-	info := editInfo{
+	ei := editInfo{
 		wordsDir:    *wordsDir,
 		shotsDir:    *shotsDir,
 		packageName: flag.Arg(0),
@@ -96,61 +89,48 @@ update images: %v
 		*credentialsJson, *wordsDir, *shotsDir, do_text, do_images)
 
 	var err error
-	info.service, err = apt.GetAPService(*credentialsJson)
+	ei.service, err = apt.GetAPService(*credentialsJson)
 	if err != nil {
 		usage(fmt.Errorf("connecting to %s got %v", *credentialsJson, err))
 	}
-	info.editId, err = apt.EditsInsert(info.service, info.packageName)
+	ei.editId, err = apt.EditsInsert(ei.service, ei.packageName)
 	if err != nil {
 		usage(fmt.Errorf("getting edits insert got %v", err))
 	}
 
 	// Details
-	appDetails, err := info.service.Edits.Details.Get(
-		info.packageName, info.editId).Do()
+	appDetails, err := ei.service.Edits.Details.Get(
+		ei.packageName, ei.editId).Do()
 	if err != nil {
-		fatal(fmt.Errorf("getting %s details got %v", info.packageName, err))
+		fatal(fmt.Errorf("getting %s details got %v", ei.packageName, err))
 	}
-	// TODO: limit this.
-	fmt.Printf("%s %s\n%s\n%s\n",
-		info.packageName, appDetails.DefaultLanguage,
+	fmt.Printf("%s default lang:%s\ne-mail:%s\nwebsite:%s\n",
+		ei.packageName, appDetails.DefaultLanguage,
 		appDetails.ContactEmail,
 		appDetails.ContactWebsite)
 	// Finish setting up info.
-	info.defBcp47 = appDetails.DefaultLanguage
+	ei.defBcp47 = appDetails.DefaultLanguage
 
-	listings, err := listings(&info)
+	listings, err := listings(&ei)
 	if err != nil {
 		fatal(err)
 	}
 
 	// Get the BCP-47 codes we need to check.
-	translateable, err := xlns.TranslateableGoogleLocales(info.wordsDir, info.defBcp47)
+	translateable, err := xlns.TranslateableGoogleLocales(ei.wordsDir, ei.defBcp47)
 	if err != nil {
 		fatal(err)
 	}
 	// We need to update the default locale also.
-	translateable = append(translateable, info.defBcp47)
-
-	// Set update limit.
-	//if *updateLimit == -1 {
-	//	*updateLimit = len(translateable)
-	//}
+	translateable = append(translateable, ei.defBcp47)
 
 	// Show the BCP-47 places we aren't checking.
-	un, err := xlns.UntranslateableGoogleLocales(info.wordsDir, info.defBcp47)
+	un, err := xlns.UntranslateableGoogleLocales(ei.wordsDir, ei.defBcp47)
 	if err == nil {
 		fmt.Printf("Can't update:%v\n", un)
 	}
 
-	/*
-		// How many calls to Google?
-		//log.Printf("%d calls", *xlns.APCalls)
-		lastCalls := *xlns.APCalls
-	*/
-
 	// By locale.
-	//needingUpdate := 0
 	for i, listing := range listings {
 		// Output BCP-47.
 		fmt.Printf("%s (%d/%d)\n", listing.Language, i+1, len(listings))
@@ -167,59 +147,30 @@ update images: %v
 			continue
 		}
 
-		//has, err := hasCountry(&info, bcp47)
-		//if err != nil {
-		//	fatal(err)
-		//}
-		//if !has {
-		//	fmt.Printf("missing %s", bcp47)
-		//	continue
-		//}
-
-		//localeNeedsUpdate := false
 		if do_text {
-			if info.defBcp47 != listing.Language {
+			if ei.defBcp47 != listing.Language {
 				// The description for the "base" locale is by definition
 				// correct (and not in need of translating!).
-				//localeNeedsUpdate = updateDescriptions(&info)
-				err := updateDescriptions(&info, listing.Language)
+				err := updateDescriptions(&ei, listing.Language)
 				if err != nil {
 					fatal(err)
 				}
 			}
 		}
 		if do_images {
-			//	localeNeedsUpdate =
-			//		localeNeedsUpdate
-			//			updateImages(*shotsDir, apei, defBcp47, bcp47) || localeNeedsUpdate
-			err := updateImages(&info, listing.Language)
+			err := updateImages(&ei, listing.Language)
 			if err != nil {
 				fatal(err)
 			}
 		}
-
-		//needsCommit = needsCommit || localeNeedsUpdate
-
-		//log.Printf("%d calls +%d", *xlns.APCalls, *xlns.APCalls-lastCalls)
-		//lastCalls = *xlns.APCalls
-
-		//if localeNeedsUpdate {
-		//	needingUpdate++
-		//	//if needingUpdate >= *updateLimit {
-		//	//	break
-		//	//}
-		//}
 	}
 
-	if info.needsCommit {
-		err := apt.EditsCommit(info.service, info.packageName, info.editId)
+	if ei.needsCommit {
+		err := apt.EditsCommit(ei.service, ei.packageName, ei.editId)
 		if err != nil {
 			fatal(err)
 		}
 	}
-
-	//log.Printf("%d calls +%d", *xlns.APCalls, *xlns.APCalls-lastCalls)
-	fmt.Println("it worked?")
 }
 
 func usage(err error) {
@@ -247,35 +198,14 @@ func listings(ei *editInfo) ([]*ap.Listing, error) {
 	return ls, nil
 }
 
-//func hasCountry(ei *editInfo, bcp47) (bool, error) {
-//	avail, err := ei.service.Edits.Countryavailability.
-//}
-
-var showBase = true
-
 // updateDescription updates the description information for a package for
 // each BCP-47 location it has information for.
 func updateDescriptions(ei *editInfo, bcp47 string) error {
 	// Get the base language listing.
 	baseListing, err := ei.service.Edits.Listings.Get(
 		ei.packageName, ei.editId, ei.defBcp47).Do()
-	//err := xlns.ExpBackoff(func() error {
-	//	var err error
-	//	baseListing, err = baseLangGet.Do()
-	//	return err
-	//})
 	if err != nil {
 		return fmt.Errorf("getting edit listing for %s got %v", bcp47, err)
-	}
-	if err != nil {
-		log.Fatalf("Edits.Listings.Get failed %v", err)
-	}
-	if showBase {
-		log.Printf("\n%s\n%s\n%s\n",
-			baseListing.Title,
-			baseListing.ShortDescription,
-			baseListing.FullDescription)
-		showBase = false
 	}
 	base639 := xlns.Iso639FromBcp47(ei.defBcp47)
 
@@ -289,14 +219,8 @@ func updateDescriptions(ei *editInfo, bcp47 string) error {
 
 	// Check if update is needed.
 	// Read existing.
-	// TODO: on failure add...
 	listing, err := ei.service.Edits.Listings.Get(
 		ei.packageName, ei.editId, bcp47).Do()
-	//err = xlns.ExpBackoff(func() error {
-	//	var err error
-	//	listing, err = langGet.Do()
-	//	return err
-	//})
 	if err != nil {
 		return fmt.Errorf("get listing for %s failed %v", bcp47, err)
 	}
@@ -313,16 +237,12 @@ func updateDescriptions(ei *editInfo, bcp47 string) error {
 		listing.ShortDescription == translated.ShortDescription &&
 		listing.FullDescription == translated.FullDescription
 	if isTheSame {
-		fmt.Printf("no changes for %s", bcp47)
+		fmt.Printf("no changes for %s\n", bcp47)
 		return nil
 	}
 
 	_, err = ei.service.Edits.Listings.Update(
 		ei.packageName, ei.editId, bcp47, &translated).Do()
-	//err = xlns.ExpBackoff(func() error {
-	//	_, err := update.Do() // *Listing, error
-	//	return err
-	//})
 	if err != nil {
 		return fmt.Errorf("listing update for %s got %v", bcp47, err)
 	}
@@ -345,24 +265,18 @@ func updateImages(ei *editInfo, bcp47 string) error {
 		locImageDir := filepath.Join(ei.shotsDir, imageType)
 		// Look for locale specific images first.
 		pattern := filepath.Join(locImageDir, bcp47+"*.png")
+		// Glob only has errors for bad patterns.
 		matches, _ := filepath.Glob(pattern)
-		//if err != nil {
-		//	log.Fatalf("Glob for %s failed %v", pattern, err)
-		//}
 		if len(matches) == 0 {
 			// Look for language specific images.
 			pattern = filepath.Join(locImageDir, iso639+"*.png")
 			matches, _ = filepath.Glob(pattern)
-			//if err != nil {
-			//	log.Fatalf("Glob for %s failed %v", pattern, err)
-			//}
 		}
 		// Get info from the directory and from Google.
 		sis, err := getShotInfo(matches)
 		if err != nil {
 			return err
 		}
-		//images := getImageInfo(ei, bcp47, imageType)
 		ilr, err := ei.service.Edits.Images.List(ei.packageName, ei.editId, bcp47, imageType).Do()
 		if err != nil {
 			return fmt.Errorf("image list for %s %s got %v",
@@ -389,9 +303,6 @@ func updateImages(ei *editInfo, bcp47 string) error {
 			fmt.Printf("delete %s %s %s\n", bcp47, imageType, doomed.Id)
 			err := ei.service.Edits.Images.Delete(
 				ei.packageName, ei.editId, bcp47, imageType, doomed.Id).Do()
-			//err := xlns.ExpBackoff(func() error {
-			//	return del.Do()
-			//})
 			if err != nil {
 				return err
 			}
@@ -412,14 +323,6 @@ func updateImages(ei *editInfo, bcp47 string) error {
 				if err != nil {
 					return fmt.Errorf("uploading %s got %v", si.file, err)
 				}
-				//_ = upload.Media(fPng)
-				//err = xlns.ExpBackoff(func() error {
-				//	_, err := upload.Do()
-				//	return err
-				//})
-				//if err != nil {
-				//	log.Fatalf("Edits.Images.Upload failed %v", err)
-				//}
 				ei.needsCommit = true
 			}
 		}
@@ -429,12 +332,6 @@ func updateImages(ei *editInfo, bcp47 string) error {
 
 func getImageInfo(ei *editInfo, bcp47, imageType string) ([]*ap.Image, error) {
 	list, err := ei.service.Edits.Images.List(ei.packageName, ei.editId, bcp47, imageType).Do()
-	//var ilr *ap.ImagesListResponse
-	//err := xlns.ExpBackoff(func() error {
-	//	var err error
-	//	ilr, err = list.Do()
-	//	return err
-	//})
 	if err != nil {
 		return nil, fmt.Errorf("image list for %s %s got %v",
 			bcp47, imageType, err)
